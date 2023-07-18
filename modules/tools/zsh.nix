cd6f5b2e9fa92e369f15a373d7bb6e9cba3dcb74
vollowx <muffinslog@proton.me>
2023-07-18 10:24:13 +0800

    feat(zsh): ctp themed

--- modules/tools/zsh.nix
+++ modules/tools/zsh.nix
@@ -92,10 +92,25 @@
 
   programs.nix-index.enable = true;
 
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
-  };
+  programs.starship =
+    let
+      flavour = "mocha";
+    in
+    {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        format = "$all";
+        palette = "catppuccin_${flavour}";
+      } // builtins.fromTOML (builtins.readFile
+        (pkgs.fetchFromGitHub
+          {
+            owner = "catppuccin";
+            repo = "starship";
+            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f";
+            sha256 = "nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
+          } + /palettes/${flavour}.toml));
+    };
 
   programs.zoxide = {
     enable = true;
