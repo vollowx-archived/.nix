0abc042149a3ba4652aba921bb609f1242a0a627
vollowx <muffinslog@proton.me>
2023-07-23 12:14:31 +0000

    feat(hm): turn foot module on

--- home-manager/vollow/sakura/home.nix
+++ home-manager/vollow/sakura/home.nix
@@ -2,7 +2,7 @@
   # You can import other home-manager modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
+    outputs.homeManagerModules.terminals.foot
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
