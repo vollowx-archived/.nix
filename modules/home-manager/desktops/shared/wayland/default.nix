10e3a2ef2e1adb7d24caea2589540eaea1c70a85
vollowx <muffinslog@proton.me>
2023-07-24 08:35:31 +0000

    fix: add wayland module

---
+++ modules/home-manager/desktops/shared/wayland/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }: {
+  home = {
+    packages = with pkgs; [wl-clipboard wlogout wofi brightnessctl wdisplays];
+    sessionVariables = {
+      # Wayland
+      NIXOS_OZONE_WL = "1";
+      QT_QPA_PLATFORM = "wayland";
+      SDL_VIDEODRIVER = "wayland";
+      CLUTTER_BACKEND = "wayland";
+      # Firefox
+      MOZ_ENABLE_WAYLAND = "1";
+      # QT
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      QT_QPA_PLATFORMTHEME = "qt6ct";
+      # Java
+      _JAVA_AWT_WM_NONREPARENTING = "1";
+    };
+  };
+
+  services.clipman = { enable = true; };
+}
