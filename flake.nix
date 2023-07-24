354a740c5369e65e6f8b2576650548b4af9c9225
vollowx <muffinslog@proton.me>
2023-07-24 11:05:25 +0000

    feat: update system version

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Flakes for Nix systems.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     hardware.url = "github:nixos/nixos-hardware";
