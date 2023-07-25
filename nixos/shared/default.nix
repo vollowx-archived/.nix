1c17652dba7027945ed299d1fc3f94ef93ef98de
vollowx <muffinslog@proton.me>
2023-07-25 11:58:06 +0800

    chore: modify locale information

--- nixos/shared/default.nix
+++ nixos/shared/default.nix
@@ -9,4 +9,15 @@
   services.v2raya.enable = true;
 
   programs.git.enable = true;
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = { LC_TIME = "en_US.UTF-8"; };
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "zh_CN.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+    ];
+  };
+  time.timeZone = "Asia/Shanghai";
 }
