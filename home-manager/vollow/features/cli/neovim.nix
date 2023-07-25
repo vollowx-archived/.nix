4f25a785f1df49e87265ed3fe486eff37ac9f622
vollowx <muffinslog@proton.me>
2023-07-25 14:51:47 +0800

    feat(neovim): add LSPs and linters

--- home-manager/vollow/features/cli/neovim.nix
+++ home-manager/vollow/features/cli/neovim.nix
@@ -1,8 +1,26 @@
-{ config, ... }: {
+{ pkgs, config, ... }: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     withNodeJs = true;
     withPython3 = true;
+    extraPackages = with pkgs; [
+      clang
+      gnumake
+
+      lua-language-server
+      nodePackages.typescript
+      nodePackages.typescript-language-server
+      nodePackages.vscode-langservers-extracted
+      nodePackages.yaml-language-server
+      marksman
+
+      nodePackages.prettier
+      stylua
+      shfmt
+      nixfmt
+
+      tree-sitter
+    ];
   };
 }
