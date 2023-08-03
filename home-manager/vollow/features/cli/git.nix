4e1b21b54db6a70c419a6ea40a4cbd00fa94d389
vollowx <muffinslog@proton.me>
2023-08-03 14:41:43 +0800

    feat(git): use modern UI

--- home-manager/vollow/features/cli/git.nix
+++ home-manager/vollow/features/cli/git.nix
@@ -1,8 +1,40 @@
 {
   programs.git = {
     enable = true;
-
+    lfs.enable = true;
+    delta = {
+      enable = true;
+      options = {
+        features = "decorations line-numbers";
+        whitespace-error-style = "22 reverse";
+        navigate = true;
+        syntax-theme = "Catppuccin-mocha";
+      };
+    };
+    extraConfig = { init = { defaultBranch = "main"; }; };
     userName = "vollowx";
     userEmail = "muffinslog@proton.me";
+  };
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      theme = let
+        green = "#a6d189";
+        text = "#c6d0f5";
+        blue = "#090909";
+        surface0 = "#414559";
+        teal = "#81c8be";
+      in {
+        lightTheme = false;
+        activeBorderColor = [ green "bold" ];
+        inactiveBorderColor = [ text ];
+        optionsTextColor = [ blue ];
+        selectedLineBgColor = [ surface0 "default" ];
+        selectedRangeBgColor = [ surface0 ];
+        cherryPickedCommitBgColor = [ teal ];
+        cherryPickedCommitFgColor = [ blue ];
+        unstagedChangesColor = [ "red" ];
+      };
+    };
   };
 }
