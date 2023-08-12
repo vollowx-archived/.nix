a1f4369e30769e68668b2e8521710af3fe8fbaa4
vollowx <muffinslog@proton.me>
2023-08-12 10:25:52 +0800

    feat(chromium): settings

--- home/vollow/features/desktops/shared/chromium.nix
+++ home/vollow/features/desktops/shared/chromium.nix
@@ -1,1 +1,34 @@
-{ programs.chromium = { enable = true; }; }
+{
+  programs.chromium = {
+    enable = true;
+    extensions = [
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+      "clngdbkpkpeebahjckkjfobafhncgmne" # stylus
+      "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+      "mnjggcdmjocbbbhaepdhchncahnbgone" # SponsorBlock
+      "jghecgabfgfdldnmbfkhmffcabddioke" # Volume Master
+    ];
+    commandLineArgs = [
+      "--force-dark-mode"
+      "--enable-gpu-rasterization"
+      "--enable-oop-rasterization"
+      "--enable-zero-copy"
+      "--ignore-gpu-blocklist"
+      "--no-default-browser-check"
+      "--no-service-autorun"
+      "--disable-features=PreloadMediaEngagementData,MediaEngagementBypassAutoplayPolicies"
+      "--disable-reading-from-canvas"
+      "--no-pings"
+      "--no-first-run"
+      "--no-experiments"
+      "--no-crash-upload"
+      "--disable-wake-on-wifi"
+      "--disable-breakpad"
+      "--disable-sync"
+      "--disable-speech-api"
+      "--disable-speech-synthesis-api"
+      "--ozone-platform=wayland"
+      "--enable-features=UseOzonePlatform,WebUIDarkMode,TouchpadOverscrollHistoryNavigation,VaapiVideoDecoder"
+    ];
+  };
+}
