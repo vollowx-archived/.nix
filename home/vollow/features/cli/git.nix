33504225ad7e574416810edfcb85004212aac5fc
vollowx <muffinslog@proton.me>
2023-08-18 13:27:11 +0800

    chore: bump flakes version

--- home/vollow/features/cli/git.nix
+++ home/vollow/features/cli/git.nix
@@ -1,13 +1,4 @@
-{ pkgs, ... }:
-let
-  fromYAML = f:
-    let
-      jsonFile =
-        pkgs.runCommand "in.json" { nativeBuildInputs = [ pkgs.jc ]; } ''
-          jc --yaml < "${f}" > "$out"
-        '';
-    in builtins.elemAt (builtins.fromJSON (builtins.readFile jsonFile)) 0;
-in {
+{
   programs.git = {
     enable = true;
     lfs.enable = true;
@@ -24,13 +15,5 @@ in {
     userName = "vollowx";
     userEmail = "muffinslog@proton.me";
   };
-  programs.lazygit = {
-    enable = true;
-    settings = { } // fromYAML (pkgs.fetchFromGitHub {
-      owner = "catppuccin";
-      repo = "lazygit";
-      rev = "f01edfd57fa2aa7cd69a92537a613bb3c91e65dd";
-      sha256 = "sha256-zjzDtXcGtUon4QbrZnlAPzngEyH56yy8TCyFv0rIbOA=";
-    } + "/themes/mocha.yml");
-  };
+  programs.lazygit = { enable = true; };
 }
